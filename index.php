e5432ca0fb266df39e4b68e83c709bdf573dd392
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2021-08-05 11:19:00 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*
+        1.sukurkite masyvą. užpildykite 30čia atsitiktinių skaičių nuo 20 iki 40.
+        1b. atspausdinkite masyvo skaičius vienoje eilutėje atsirtus tarpais.
+
+        2. sukurkite masyvą su 10 skaičių nuo 0 iki 9
+        2b. sukurkite kitą masyvą su 10 skaičių nuo 10 iki 19
+        2c. pasinaudoję ciklu atspausdinkite 100 atsitiktinių skaičių iš 2b masyvo.
+        t.y kiekvienoje ciklo iteracijoje imkite atsitiktinį 2b masyvo skaičių.
+
+        3. sukurkite dvimatį masyvą. pirmas matmuo - 10 elementų. antras 10 elementų.
+        reikšmės - skaičiai nuo 0 iki 9.
+        3b. atspausdinkite duomenis. (gausis skaičių stačiakampis).
+        3c. padarykite tą patį, ką  darėte 3b, tik spausdinkite tuos skaičius pakeltus kvadratu
+
+        
+        4. sukurkite masyvą pavadinimu $gyvunas; suteikite jam reikšmes key + value principu.
+        raktažodžiai (key): "species","age", "name", "description". Tai bus vienas objektas,
+        vienas gyvūnas, su rūšimi, amžiumi, vardu ir aprašu.
+        
+        5. sukurkite tris masyvus:
+        $names = ["vardas1","vardas2","vardas3"....] vardus sugalvokite patys. jų reikia 10.
+        $species = ["kengūra","gazelė","liūtas"....] gyvūnus sugalvokite patys. jų reikia 10.
+        $descriptions = ["gražus bet piktas","pastoviai miega","šauniai maskuojasi"....] 
+        aprašus sugalvokite patys. jų reikia 10.
+
+        5b. sukurkite dvimatį masyvą iš 20 elementų.  didžiąjame masyve bus sudėti mažesni masyvai,
+         iš eilės. maži masyvai bus tokie patys kaip 4. užduotyje. jų reikšmes užpildykite 5. 
+         masyvų reikšmėmis. t.y imate random vardą, ir priskiriate konstruojamam gyvūnui ir tt.
+        
+        5c. atvaizduokite gyvūnus iš 5b masyvo. vienas gyvūnas - viena eilutė.
+
+        6. padarykite daugybos lentelę nuo 1 iki 9.
+
+        =======================
+        funkcijos
+        =======================
+
+        1. sukurkite funkciją kuri išspausdina kokį nors tekstą.
+        1b. sukurkite funkciją kuri priima vieną parametrą ir išspausdina tekstas "labas" +
+         tas paduotas kintamasis
+        1c. sukurkite funkciją kuri gražina skaičiaus PI pirmus 8 skaičius
+        1d. sukurkite funkciją kuri gražina dviejų parametrų sąndaugą.
+        1e. sukurkite funkciją kuri apskaičiuoja skritulio PLOTĄ. ši funkcija priima 1 parametrą
+        bei naudoja 1c. funkciją savo viduje, kad gautų PI reikšmę.
+        1f. sukurkite funkciją kuri apskaičiuoja skritulio ILGĮ. ši funkcija priima 1 parametrą
+        bei naudoja 1c. funkciją savo viduje, kad gautų PI reikšmę.
+        1g. sukurkite funkciją kuri: priima vieną parametrą. savyje iškviecia 1e ir 1f funkcijas
+        ir atspausdina:
+         apskritimo ilgis: (tai ka gražino funkcija 1f)
+         apskritimo plotas: (tai ka gražino funkcija 1g)
+
+
+
+
+
+    */
+
+
+
+
+    ?>
+</body>
+</html>
